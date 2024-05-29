374527351ab7173cdbcbab188fd6915bbba2e1cc
saku-bruh <saku-bruh@proton.me>
2024-05-29 20:02:22 +0300

    extra: audio: Switch to stock Sound Enhancements
    * Imported from:
    https://github.com/Ahnet-69/vendor_sony_extra/
    
    * Added 1 III dax-default.xml
    
    * Added hacks for 360RA to work out of the box
    
    * Thanks to:
    hellobbn - Original port
    ahnet-69 - Backport to MK2
    someone5678 - Modfied Dolby APKs to add support for a QS tile
    saku-bruh - Provided some fixes for 360RA & some other stuff
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>
    Co-Authored-By: hellobbn <10683168+hellobbn@users.noreply.github.com>
    Co-Authored-By: someone5678 <59456192+someone5678@users.noreply.github.com>
    Co-Authored-By: Ahnet <57623523+ahnet-69@users.noreply.github.com>

--- Sagami/extra.mk
+++ Sagami/extra.mk
@@ -45,7 +45,7 @@ endif
 
 # Sound Enhancements
 ifeq ($(TARGET_SHIPS_SOUND_ENHANCEMENT),true)
-    $(call inherit-product, vendor/sony/extra/Sagami/audio/dolby/dolby.mk)
+    $(call inherit-product, vendor/sony/extra/Sagami/audio/audio.mk)
 endif
 
 # Game Controllers
